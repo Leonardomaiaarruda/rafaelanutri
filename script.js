5d8031e6b5bf66e6d161ebe8f6dd0fcfb154d8ba
Leonardo m <leonardomaiaarruda@gmail.com>
2024-12-15 22:22:49 -0300

    name rafaela nutri

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// TMB = 655.1 + (9.563 * peso) + (1.850 * altura) - (4.676 * idade)
+TMB = 655.1 + (9.563 * peso) + (1.850 * altura) - (4.676 * idade)
 
 const form = document.getElementById('tmbForm');
 const resultDiv = document.getElementById('result');
@@ -11,10 +11,14 @@ form.addEventListener('submit', function(event) {
     event.preventDefault(); // Evita o recarregamento da página
 
     // Captura os valores do formulário
-    const weight = parseFloat(document.getElementById('weight').value);
-    const height = parseFloat(document.getElementById('height').value);
-    const age = parseInt(document.getElementById('age').value);
+    let weight = parseFloat(document.getElementById('weight').value.replace(',', '.'));
+    let height = parseFloat(document.getElementById('height').value);
+    let age = parseInt(document.getElementById('age').value.replace(',', '.'));
     const activityLevel = parseFloat(document.getElementById('activity').value);
+
+    // Define valores padrão se os campos estiverem vazios
+    if (isNaN(weight)) weight = 60; // Peso padrão: 60kg
+    if (isNaN(age)) age = 30; // Idade padrão: 30 anos
 
     // Calcula o TMB usando a fórmula de Harris-Benedict
     const tmb = 655.1 + (9.563 * weight) + (1.850 * height) - (4.676 * age);
@@ -23,7 +27,7 @@ form.addEventListener('submit', function(event) {
     const adjustedTMB = tmb * activityLevel;
 
     // Exibe o resultado no modal
-    modalResult.textContent = `Sua TMB ajustada é ${adjustedTMB.toFixed(2)} kcal/dia.`;
+    modalResult.textContent = `Suya necessidade de calorias diariamente é de ${adjustedTMB.toFixed(2)} kcal/dia.`;
     modal.style.display = 'flex';
 });
 
@@ -32,5 +36,5 @@ closeModal.addEventListener('click', function() {
 });
 
 purchaseButton.addEventListener('click', function() {
-    window.location.href = 'https://wa.me/5544988057085?text=ol%C3%A1%2C%20gostaria%20de%20saber%20mais%20sobre%20o%20cardio%20personalizado';
+    window.location.href = 'https://wa.me/5544988057085?text=Ol%C3%A1%2Cgostaria%20de%20saber%20mais%20sobre%20o%20cardapio%20personalizado';
 });
